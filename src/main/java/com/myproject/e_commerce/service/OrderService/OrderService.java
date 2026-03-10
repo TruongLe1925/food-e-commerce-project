9e5d225115ebc030d833099c09b2c6efa1166ef4
TruongLe1925 <lephuoctruongppp@gmail.com>
2026-03-10 08:05:31 +0700

    fix frontend

--- src/main/java/com/myproject/e_commerce/service/OrderService/OrderService.java
+++ src/main/java/com/myproject/e_commerce/service/OrderService/OrderService.java
@@ -4,18 +4,16 @@ import com.myproject.e_commerce.constants.StatusOrder;
 import com.myproject.e_commerce.dto.OrderDTO;
 import com.myproject.e_commerce.dto.OrderDetailsWrapperDTO;
 import com.myproject.e_commerce.dto.OrderProcessDTO;
-import com.myproject.e_commerce.dto.ProductHomePageDTO;
 
 import java.util.List;
 
 public interface OrderService {
     void addToOrder(String username, String note);
     List<OrderDTO> getOrder(String username);
-    OrderDetailsWrapperDTO getOrderDetails(Integer orderId,String username);
+    OrderDetailsWrapperDTO getOrderDetails(Integer orderId, String username);
     OrderDetailsWrapperDTO getOrderDetailsForEmployee(Integer orderId);
     List<OrderProcessDTO> getOrderProcess();
     List<OrderProcessDTO> getOrderProcessByStatus(StatusOrder status);
     void updateToNextStatus(Integer orderId);
     void cancelOrder(Integer orderId);
-
 }
