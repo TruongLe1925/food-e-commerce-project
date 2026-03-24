aad26500b89b41d3aa71c09cacf6bba4ea0962d1
TruongLe1925 <lephuoctruongppp@gmail.com>
2026-03-25 01:02:26 +0700

    add:Exception

---
+++ src/main/java/com/myproject/e_commerce/exception/exception/PromotionExpiredException.java
@@ -0,0 +1,7 @@
+package com.myproject.e_commerce.exception.exception;
+
+public class PromotionExpiredException extends RuntimeException {
+    public PromotionExpiredException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/myproject/e_commerce/restController/UserRestController.java
@@ -0,0 +1,9 @@
+package com.myproject.e_commerce.restController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserRestController {
+}
